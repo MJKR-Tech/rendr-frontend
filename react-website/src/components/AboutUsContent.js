84aa2d47f4a7b33c4b28d2a9f6cd4acf066c416b
Timothy Yang <timothyoung97@gmail.com>
2022-04-06 16:07:11 +0800

    Update BG image

--- react-website/src/components/AboutUsContent.js
+++ react-website/src/components/AboutUsContent.js
@@ -5,7 +5,6 @@ import './FunctionSection.css';
 function FunctionSection() {
   return (
     <div className='function-container'>
-        <video src='/videos/video-2.mp4' autoPlay muted loop />
         <h1>Team Member</h1>
         <p>We are the Champion</p>
     </div>
